399f13ca310a564c617c5b4307c442c0627b605a
dhwisema <dhwisema@ncsu.edu>
2026-07-21 10:37:07 -0400

    bluetooth kinda not happy with this

--- Modules/Desktop/Airpods-fix.nix
+++ Modules/Desktop/Airpods-fix.nix
@@ -5,15 +5,15 @@
   ...
 }:
 {
-  hardware.bluetooth = {
-    enable = true;
-    powerOnBoot = true;
-    settings = {
-      General = {
-        # Spoof Apple Vendor ID for advanced AirPods features
-        DeviceID = "bluetooth:004C:0000:0000";
-      };
-    };
-  };
+  # hardware.bluetooth = {
+  #   enable = true;
+  #   powerOnBoot = true;
+  #   settings = {
+  #     General = {
+  #       # Spoof Apple Vendor ID for advanced AirPods features
+  #       DeviceID = "bluetooth:004C:0000:0000";
+  #     };
+  #   };
+  # };
   environment.systemPackages = with pkgs; [ librepods ];
 }
