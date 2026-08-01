76d82c2ea7c323e03ff6dad56d26ad426f02bb62
dhwisema <dhwisema@ncsu.edu>
2026-08-01 00:19:14 -0400

    change book place

--- Modules/Containers/CWA.nix
+++ Modules/Containers/CWA.nix
@@ -10,17 +10,17 @@
     image = "crocodilestick/calibre-web-automated:latest";
 
     environment = {
-      PUID = "1020";
-      PGID = "1020";
+      PUID = "1000";
+      PGID = "1000";
       TZ = "America/New_York";
       HARDCOVER_TOKEN = "";
       CWA_PORT_OVERRIDE = "8083";
       NETWORK_SHARE_MODE = "false";
     };
     volumes = [
-      "/data/CWA/config:/config"
-      "/data/CWA/ingest:/cwa-book-ingest"
-      "/data/CWA/library:/calibre-library"
+      "/home/irrelevancy/CWA/config:/config"
+      "/home/irrelevancy/ingest:/cwa-book-ingest"
+      "/home/irrelevancy/CWA/library:/calibre-library"
     ];
     ports = [ "8083:8083" ];
   };
