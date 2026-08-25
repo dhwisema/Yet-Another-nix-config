cd7eab7915335feab1b72d8b601db21960369a05
dhwisema <dhwisema@ncsu.edu>
2026-08-25 15:36:26 -0400

    move jester vfio code to seperate file

--- Host/Jester/configuration.nix
+++ Host/Jester/configuration.nix
@@ -18,40 +18,7 @@
   };
   boot.kernelParams = [
     "amdgpu.dcdebugmask=0x10"
-    "amd_iommu=on"
-    "iommu=pt"
   ]; # disable psr-su
-  boot.extraModulePackages = [ config.boot.kernelPackages.kvmfr ];
-  boot.kernelModules = [
-    "vfio"
-    "vfio_iommu_type1"
-    "vfio_pci"
-    "kvmfr"
-  ];
-  boot.extraModprobeConfig = ''
-    options kvmfr static_size_mb=64
-  '';
-
-services.udev.extraRules = ''
-    SUBSYSTEM=="kvmfr", OWNER="irrelevancy", GROUP="kvm", MODE="0660"
-  '';
-systemd.tmpfiles.rules = [
-  "f /dev/shm/looking-glass 0660 root kvm -"
-];
-  environment.systemPackages = [
-    pkgs.qemu
-    pkgs.looking-glass-client
-  ];
-  programs.virt-manager.enable = true;
-  virtualisation.spiceUSBRedirection.enable = true;
-  virtualisation.libvirtd = {
-    enable = true;
-    qemu = {
-      package = pkgs.qemu_kvm;
-      runAsRoot = true;
-      swtpm.enable = true;
-    };
-  };
   systemd.services.wifi-fix = {
     enable = true;
     after = [
