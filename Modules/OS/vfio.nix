d90a05a7ee00762d4047a481bdd76ecb8dc51691
dhwisema <dhwisema@ncsu.edu>
2026-08-26 11:38:56 -0400

    another vfio

--- Modules/OS/vfio.nix
+++ Modules/OS/vfio.nix
@@ -14,15 +14,14 @@
       runAsRoot = true;
       swtpm.enable = true;
       verbatimConfig = ''
-            namespaces = []
+        namespaces = []
         cgroup_device_acl = [
           "/dev/null", "/dev/full", "/dev/zero",
           "/dev/random", "/dev/urandom",
           "/dev/ptmx", "/dev/kvm", "/dev/rtc",
           "/dev/hpet", "/dev/vfio/vfio", "/dev/kvmfr0"
         ]
-
-            virtiofsd = "${pkgs.virtiofsd}/bin/virtiofsd"
+        virtiofsd = "${pkgs.virtiofsd}/bin/virtiofsd"
       '';
     };
   };
@@ -60,6 +59,12 @@
   systemd.tmpfiles.rules = [
     "f /dev/shm/looking-glass 0660 root kvm -"
   ];
+
+  system.activationScripts.virtiofsd = ''
+  mkdir -p /usr/libexec /usr/lib
+  ln -sfn ${pkgs.virtiofsd}/bin/virtiofsd /usr/libexec/virtiofsd
+  ln -sfn ${pkgs.virtiofsd}/bin/virtiofsd /usr/lib/virtiofsd
+'';
 
   # 4. Add your user to necessary groups
   users.users.irrelevancy.extraGroups = [
